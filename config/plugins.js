1838a9c092189febc77ae37210a9fa32c0ee8b44
newuser <yespeace@bk.ru>
2024-02-11 21:22:23 +0600

    fx

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,7 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    "users-permissions": {
+      config: {
+        jwtSecret: '90a2cd81793347d62df1bde5297501f1762687f87026099ed38e76db91173aa73ef114fa03e7076fce18c82e832cdd695ff1fd35db472845f45bb4c54bed1aba',
+      }
+    }
+  })
